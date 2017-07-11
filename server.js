62651396feda11dd10a882730fe26411790cda17
Djuro Vujatovic <djuro.vujatovic@milestonetech.com>
2017-07-11 12:57:29 -0600

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -58,4 +58,4 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(port);
